72d2c3689a9683fb39221b327a9a8fc25ca4835f
Adam Yuan <adamyuanprofessional@gmail.com>
2024-07-25 17:00:28 +0100

    Updated contact

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\30120\Documents\Programming Projects\django_web_projects\django-pixel\venv
+set VIRTUAL_ENV=C:\Users\30120\Documents\Programming Projects\django_web_projects\study_smart_tutors\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
